348e6b88639ebaa8e5ae8c194ab0ff621bb11826
xli <xgng_l@yahoo.com>
2016-02-24 15:29:55 -0500

    Added ExecutorDemo

---
+++ src/demo/concurrent/executor/ExecutorDemo.java
@@ -0,0 +1,35 @@
+package demo.concurrent.executor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorDemo {
+
+	public static void main(String[] args) {
+		ExecutorService pool = Executors.newFixedThreadPool(3);
+		
+		List<Future<String>> list = new ArrayList<>();
+		for(int i=0; i< 10; i++){
+			Callable<String> callable = new RandomNumberCallable(i);
+            Future<String> future = pool.submit(callable);
+            list.add(future);
+        }
+		
+        for(Future<String> fut : list){
+            try {
+                System.out.println(fut.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        pool.shutdown();
+        
+	}
+
+}
---
+++ src/demo/concurrent/executor/RandomNumberCallable.java
@@ -0,0 +1,23 @@
+package demo.concurrent.executor;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class RandomNumberCallable implements Callable<String> {
+	
+	private int id;
+	
+	public RandomNumberCallable(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String call() throws Exception {
+		System.out.println(String.format("Callable ID %s in %s started.", id, Thread.currentThread().getName()));
+		Thread.sleep(500);
+		Random random = new Random();
+		int number = random.nextInt(100);
+		return String.format("Callable ID %s returned %s", id, number);
+	}
+
+}
