513e887f7d694fa7473432210ed3f1b7fdbdf592
xli <xgng_l@yahoo.com>
2016-11-24 14:38:24 -0500

    Fix SpiralOrderMatrix

--- src/programming/array/SpiralOrderMatrix.java
+++ src/programming/array/SpiralOrderMatrix.java
@@ -19,22 +19,22 @@ public class SpiralOrderMatrix {
 		 int bottom = rows - 1;
 		 
 		 while (top <= bottom && left <= right) {
-			 for (int i = left; i <= right; i++) {
+			 for (int i = left; i <= right && top <= bottom; i++) {
 				 result.add(a.get(top).get(i));
 			 }
 			 top += 1;
 			 
-			 for (int i = top; i <= bottom; i++) {
+			 for (int i = top; i <= bottom && left <= right; i++) {
 				 result.add(a.get(i).get(right));
 			 }
 			 right -= 1;
 			 
-			 for (int i = right; i >= left; i--) {
+			 for (int i = right; i >= left && top <= bottom; i--) {
 				 result.add(a.get(bottom).get(i));
 			 }
 			 bottom -= 1;
 			 
-			 for (int i = bottom; i >= top; i--) {
+			 for (int i = bottom; i >= top && left <= right; i--) {
 				 result.add(a.get(i).get(left));
 			 }
 			 left += 1;
@@ -45,10 +45,18 @@ public class SpiralOrderMatrix {
 	}
 	
 	public static void main(String[] args) {
-		List<List<Integer>> matrix = Arrays.asList(
+		List<List<Integer>> matrix1 = Arrays.asList(
 				Arrays.asList(1, 2, 3),
 				Arrays.asList(4, 5, 6),
 				Arrays.asList(7, 8, 9));
-		System.out.println(spiralOrder(matrix));
+		List<List<Integer>> matrix2 = Arrays.asList(
+				Arrays.asList(1, 2, 3));
+		List<List<Integer>> matrix3 = Arrays.asList(
+				Arrays.asList(1),
+				Arrays.asList(4),
+				Arrays.asList(7));
+		System.out.println(spiralOrder(matrix1));
+		System.out.println(spiralOrder(matrix2));
+		System.out.println(spiralOrder(matrix3));
 	}
 }
