5bb93472d33b31791ca073954c7aa0938cf91bb1
xli <xgng_l@yahoo.com>
2016-12-06 17:00:04 -0500

    Initial commit for BalancedBracket

---
+++ src/programming/datastructure/BalancedBrackets.java
@@ -0,0 +1,49 @@
+package programming.datastructure;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/balanced-brackets
+public class BalancedBrackets {
+    
+    static boolean isBalanced(String brackets) {
+        Deque<String> stack = new ArrayDeque<String>();
+        
+        for (int i = 0; i < brackets.length(); i++) {
+            String currentBracket = String.valueOf(brackets.charAt(i));
+            if (currentBracket.equals("{") || currentBracket.equals("[") || currentBracket.equals("(")) {
+                stack.push(currentBracket);
+            } else {
+                String potentialMatchingBracket = stack.pop();
+                if (currentBracket.equals("}") && !potentialMatchingBracket.equals("{")) {
+                    return false;
+                } else if (currentBracket.equals("]") && !potentialMatchingBracket.equals("[")) {
+                    return false;
+                } else if (currentBracket.equals(")") && !potentialMatchingBracket.equals("(")) {
+                    return false;
+                }
+            }
+        }
+        
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int i = 0; i < t; i++){
+            String s = in.next();
+            if (isBalanced(s)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        in.close();
+
+    }
+
+}
