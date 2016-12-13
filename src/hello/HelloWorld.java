1316f2bb971bd04d6b343edd9e2afa8cc5897cd7
superm1a3 <xgng_l@yahoo.com>
2016-12-13 17:32:12 -0500

    Finished

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -1,18 +1,35 @@
 package hello;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.google.common.collect.Lists;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        List<String> colors = Lists.newArrayList("red", "green", "blue");
-        System.out.println(colors);
+        List<String> words = Arrays.asList("Hello", "World");
         
-        List<String> red = colors.stream().filter(s -> s.equals("red")).collect(Collectors.toList());
-        System.out.println(red);
+        List<String> result = words.stream()
+                                   .map(word -> word.split(""))
+                                   .flatMap(arr -> Arrays.stream(arr))
+                                   .collect(Collectors.toList());
+        
+        result.forEach(s -> System.out.println(s.toString()));
+        System.out.println(result);
+        
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+        List<int[]> pairs =
+            numbers1.stream()
+                    .flatMap(i -> numbers2.stream()
+                                          .map(j -> new int[]{i, j})
+                                          .filter(a -> (a[0] + a[1]) % 3 == 0)
+                            )
+                    .collect(Collectors.toList());
+        
+        pairs.forEach(a -> System.out.println(a[0] + " " + a[1]));
+        
+        
     }
 
 }
--- src/programming/datastructure/Waiter.java
+++ src/programming/datastructure/Waiter.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 // https://www.hackerrank.com/challenges/waiter
 public class Waiter {
     
+    // Sieve of Eratosthenes. Not used here because we need the list the have n prime numbers,
+    // not up to n.
     static List<Integer> primeNumbersUpTo(int n) {
         List<Integer> primeNumbers = new ArrayList<>();
         boolean isPrime[] = new boolean[n+1];
@@ -34,28 +36,63 @@ public class Waiter {
         
         return primeNumbers;
     }
+    
+    static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    static List<Integer> primeNumbersWithCount(int n) {
+        List<Integer> primeNumbers = new ArrayList<>();
+        int num = 2;
+        
+        while (primeNumbers.size() < n) {
+            if (isPrime(num)) {
+                primeNumbers.add(num);
+            }
+            num++;
+        }
+        
+        return primeNumbers;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numPlates = scanner.nextInt();
         int numIterations = scanner.nextInt();
-        Deque<Integer> a0 = new ArrayDeque<>();
+        Deque<Integer> aCurrent = new ArrayDeque<>();
         
         for (int i = 0; i < numPlates; i++) {
-            a0.push(scanner.nextInt());
+            aCurrent.push(scanner.nextInt());
         }
         
-        List<Integer> primeNumbers = primeNumbersUpTo(numIterations);
+        List<Integer> primeNumbers = primeNumbersWithCount(numIterations);
         
         for (int i = 1; i <= numIterations; i++) {
+            Deque<Integer> aNext = new ArrayDeque<>();
             Deque<Integer> b = new ArrayDeque<>();
-            for (Integer num : a0) {
+            
+            while (!aCurrent.isEmpty()) {
+                Integer num = aCurrent.pop();
                 if (num % primeNumbers.get(i-1) == 0) {
                     b.push(num);
                 } else {
-                    
+                    aNext.push(num);
                 }
             }
+            
+            aCurrent = aNext;
+            while (!b.isEmpty()) {
+                System.out.println(b.pop());
+            }
+        }
+        
+        while (!aCurrent.isEmpty()) {
+            System.out.println(aCurrent.pop());
         }
         
         scanner.close();
