b6bc26af8c451f296ea475aec6ee4bc515695085
xli <xgng_l@yahoo.com>
2016-12-14 23:43:23 -0500

    Finished

--- src/programming/datastructure/QueueUsingTwoStacks.java
+++ src/programming/datastructure/QueueUsingTwoStacks.java
@@ -2,6 +2,7 @@ package programming.datastructure;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Scanner;
 
 public class QueueUsingTwoStacks {
     
@@ -15,6 +16,7 @@ public class QueueUsingTwoStacks {
                 while (!outStack.isEmpty()) {
                     inStack.push(outStack.pop());
                 }
+                inMode = true;
             }
             
             inStack.push(i);
@@ -25,13 +27,77 @@ public class QueueUsingTwoStacks {
                 while (!inStack.isEmpty()) {
                     outStack.push(inStack.pop());
                 }
+                inMode = false;
             }
             
             return outStack.pop();
         }
+        
+        void printFirst() {
+            if (inMode) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
+                }
+                inMode = false;
+            }
+            
+            System.out.println(outStack.peek());
+        }
+    }
+    
+    static class BetterQueue<T> {
+        Deque<T> inStack = new ArrayDeque<>();
+        Deque<T> outStack = new ArrayDeque<>();
+        
+        void enqueue(T i) {
+            inStack.push(i);
+        }
+        
+        T dequeue() {
+            if (outStack.isEmpty()) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+            
+            return outStack.pop();
+        }
+        
+        void printFirst() {
+            if (outStack.isEmpty()) {
+                while (!inStack.isEmpty()) {
+                    outStack.push(inStack.pop());
+                }
+            }
+            
+            System.out.println(outStack.peek());
+        }
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOps = scanner.nextInt();
+        BetterQueue<Integer> queue = new BetterQueue<Integer>();
+        
+        for (int i = 1; i <= numOps; i++) {
+            int opType = scanner.nextInt();
+            switch (opType) {
+                case 1:
+                    queue.enqueue(scanner.nextInt());
+                    break;
+                case 2:
+                    queue.dequeue();
+                    break;
+                case 3:
+                    queue.printFirst();
+                    break;
+                default:
+                    break;
+                
+            }
+        }
+        
+        scanner.close();
 
     }
 
