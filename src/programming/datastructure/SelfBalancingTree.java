76bb26b5886002fc5d48c09acde441ce3e4a6700
superm1a3 <xgng_l@yahoo.com>
2017-01-19 15:03:14 -0500

    Work In Progress

--- src/programming/datastructure/SelfBalancingTree.java
+++ src/programming/datastructure/SelfBalancingTree.java
@@ -1,5 +1,8 @@
 package programming.datastructure;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 // https://www.hackerrank.com/challenges/self-balancing-tree
 public class SelfBalancingTree {
 
@@ -11,7 +14,7 @@ public class SelfBalancingTree {
     }
     
     // Assumption: node, rightChild cannot be null; parent can be null
-    void leftRotate(Node node, Node rightChild, Node parent) {
+    static void leftRotate(Node node, Node rightChild, Node parent) {
         node.right = rightChild.left;
         rightChild.left = node;
         
@@ -26,7 +29,7 @@ public class SelfBalancingTree {
     }
     
     // Assumption: node, leftChild cannot be null; parent can be null
-    void rightRotate(Node node, Node leftChild, Node parent) {
+    static void rightRotate(Node node, Node leftChild, Node parent) {
         node.left = leftChild.right;
         leftChild.right = node;
         
@@ -39,14 +42,22 @@ public class SelfBalancingTree {
         }
     }
     
-    // TODO: if parent is null for left or right rotate, it means node is the root; need to
-    // set the root pointer accordingly in these cases.
-    
-    public static void main(String[] args) {
+    static void updateHeight(Node node) {
+        if (node != null) {
+            int leftHeight = -1;
+            int rightHeight = -1;
 
+            if (node.left != null) {
+                leftHeight = node.left.ht;
+            }
+            
+            if (node.right != null) {
+                rightHeight = node.right.ht;
+            }
+            
+            node.ht = Math.max(leftHeight, rightHeight) + 1;
+        }
     }
-    
-
     
     static boolean isNodeBalanced(Node node) {
         if (node == null) {
@@ -58,61 +69,73 @@ public class SelfBalancingTree {
             return balanceFactor <= 1 && balanceFactor >= -1; 
         }
     }
+    
+    
+    // TODO: if parent is null for left or right rotate, it means node is the root; need to
+    // set the root pointer accordingly in these cases.
+    
+    public static void main(String[] args) {
 
-    static void reBalance(Node greatGrand, Node grandParent, Node parent, Boolean greatGrandLeft, Boolean grandParentLeft, Boolean parentLeft) {
-        if (grandParentLeft == true && parentLeft == true) {
-            grandParent.left = parent.right;
-            parent.right = grandParent;
-            grandParent.ht -= 1;
-            parent.ht += 1;
-            
-            if (greatGrand != null) {
-                if (greatGrandLeft) {
-                    greatGrand.left = parent;
-                } else {
-                    greatGrand.right = parent;
-                }
-            }
-            
-            return;
-        } else if (grandParentLeft == true && parentLeft == false) {
-            Node current = parent.right;
-            parent.right = current.left;
-            current.left = parent;
-            grandParent.left = current;
-            parent.ht -= 1;
-            current.ht += 1;
-            
-            reBalance(greatGrand, grandParent, current, greatGrandLeft, true, true);
-        } else if (grandParentLeft == false && parentLeft == true) {
-            Node current = parent.left;
-            parent.left = current.right;
-            current.right = parent;
-            grandParent.right = current;
-            parent.ht -= 1;
-            current.ht += 1;
-            
-            reBalance(greatGrand, grandParent, current, greatGrandLeft, false, false);
-        } else {
-            grandParent.right = parent.left;
-            parent.left = grandParent;
-            grandParent.ht -= 1;
-            parent.ht += 1;
-            
-            if (greatGrand != null) {
-               if (greatGrandLeft) {
-                    greatGrand.left = parent;
-                } else {
-                    greatGrand.right = parent;
-                }
-            }
-            
-            return;
-        }
+    }
+    
+
+    
+
+    static void reBalance(Node newNode, Deque<Node> path) {
+//        if (grandParentLeft == true && parentLeft == true) {
+//            grandParent.left = parent.right;
+//            parent.right = grandParent;
+//            grandParent.ht -= 1;
+//            parent.ht += 1;
+//            
+//            if (greatGrand != null) {
+//                if (greatGrandLeft) {
+//                    greatGrand.left = parent;
+//                } else {
+//                    greatGrand.right = parent;
+//                }
+//            }
+//            
+//            return;
+//        } else if (grandParentLeft == true && parentLeft == false) {
+//            Node current = parent.right;
+//            parent.right = current.left;
+//            current.left = parent;
+//            grandParent.left = current;
+//            parent.ht -= 1;
+//            current.ht += 1;
+//            
+//            reBalance(greatGrand, grandParent, current, greatGrandLeft, true, true);
+//        } else if (grandParentLeft == false && parentLeft == true) {
+//            Node current = parent.left;
+//            parent.left = current.right;
+//            current.right = parent;
+//            grandParent.right = current;
+//            parent.ht -= 1;
+//            current.ht += 1;
+//            
+//            reBalance(greatGrand, grandParent, current, greatGrandLeft, false, false);
+//        } else {
+//            grandParent.right = parent.left;
+//            parent.left = grandParent;
+//            grandParent.ht -= 1;
+//            parent.ht += 1;
+//            
+//            if (greatGrand != null) {
+//               if (greatGrandLeft) {
+//                    greatGrand.left = parent;
+//                } else {
+//                    greatGrand.right = parent;
+//                }
+//            }
+//            
+//            return;
+//        }
     }
 
-    static Node insert(Node root,int val)
+    static Node insert(Node root, int val)
     {
+        Deque<Node> path = new ArrayDeque<>();
         Node newNode = new Node();
         newNode.val = val;
         newNode.ht = 0;
@@ -121,39 +144,37 @@ public class SelfBalancingTree {
             return newNode;
         } else {
             Node currentNode = root;
-            Node parent = null;
-            Node grandParent = null;
-            Node greatGrand = null;
-            Boolean parentLeft = null;
-            Boolean grandParentLeft = null;
-            Boolean greatGrandLeft = null;
             
             while (currentNode != null) {
-                greatGrand = grandParent;
-                grandParent = parent;
-                parent = currentNode;
-                currentNode.ht += 1;
-                greatGrandLeft = grandParentLeft;
-                grandParentLeft = parentLeft;
+                path.push(currentNode);
+                
                 if (newNode.val < currentNode.val) {
-                    parentLeft = true;
                     currentNode = currentNode.left;
                 } else {
-                    parentLeft = false;
                     currentNode = currentNode.right;
                 }
             }
             
-            if (parentLeft) {
+            Node parent = path.peek();
+            if (parent.val > newNode.val) {
                 parent.left = newNode;
             } else {
                 parent.right = newNode;
             }
             
-            if (grandParent != null) {
-                if (!isNodeBalanced(parent) || !isNodeBalanced(grandParent) || !isNodeBalanced(greatGrand)) {
-                    reBalance(greatGrand, grandParent, parent, greatGrandLeft, grandParentLeft, parentLeft);
+            
+            path.stream().forEach(n -> updateHeight(n));
+            
+            boolean treeBalanced = true;
+            for (Node n : path) {
+                if (!isNodeBalanced(n)) {
+                    treeBalanced = false;
+                    break;
                 }
+            }
+            
+            if (!treeBalanced) {
+                reBalance(newNode, path);
             }
             
             return root;
