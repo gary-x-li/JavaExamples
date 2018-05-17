739e3cab3d8717146598db137ecec1c01fce437f
Gary Li <xgng_l@yahoo.com>
2018-05-16 23:41:13 -0400

    Add threading demos

---
+++ src/main/java/demo/concurrent/thread/UserDaemonThreadDemo.java
@@ -0,0 +1,21 @@
+package demo.concurrent.thread;
+
+class UserDaemonThreadDemo {
+    public static void main(String[] args) {
+        MyThread mt = new MyThread();
+        //mt.setDaemon(true);
+        mt.start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Daemon is " + isDaemon());
+        while (true) ;
+    }
+}
